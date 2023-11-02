8a6548142323cd6e90ec5d4895ae85db626dd1b6
Vertigo15937 <zhzh4040@naver.com>
2023-11-02 16:56:38 +0900

    asset update

--- Assets/Exports/Project_Utility/GiantParticleGames/InspectorGraph/Editor/Assembly.cs
+++
@@ -1,8 +0,0 @@
-// ********************************
-// (C) 2023 - Giant Particle Games
-// All rights reserved.
-// ********************************
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("GiantParticle.InspectorGraphPro")]
