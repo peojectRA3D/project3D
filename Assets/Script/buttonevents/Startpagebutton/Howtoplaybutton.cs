76c5ef2d1d0420464f6370f7c2fc06b7cecf4f52
Bansh22 <33442058+Bansh22@users.noreply.github.com>
2023-11-10 16:08:14 +0900

    ㅇㄺ

--- Assets/Script/buttonevents/Startpagebutton/Howtoplaybutton.cs
+++ Assets/Script/buttonevents/Startpagebutton/Howtoplaybutton.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Howtoplaybutton : MonoBehaviour
 {
-   
-    
+
+    public mainpanelbutton mainpanel;
     public void closehowtoplaybuttonclick()
     {
         gameObject.SetActive(false);
@@ -14,6 +14,7 @@ public class Howtoplaybutton : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
+            mainpanel.butttononoff(true);
             closehowtoplaybuttonclick();
         }
     }
