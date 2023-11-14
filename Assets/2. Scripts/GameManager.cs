debd17469e803a3927d92b9516556ce94c3ad746
SpringYong <82130321+SpringYong@users.noreply.github.com>
2023-11-14 19:40:31 +0900

    Revert "Option UI"
    This reverts commit 2db7a324f91c8398512b73b29237f37fcf0095c5.

--- Assets/2. Scripts/GameManager.cs
+++ Assets/2. Scripts/GameManager.cs
@@ -6,10 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // 옵션 UI
-    public RectTransform optionUI;
-    public RectTransform optionSoundUI;
-
     // 보스 HP
     public Boss boss;
     public RectTransform bossHealth;
@@ -20,11 +16,6 @@ public class GameManager : MonoBehaviour
 
     // 패배 UI
     public Image defeat;
-
-    public void OnClickSkipButton() // 스토리 스킵
-    {
-        SceneManager.LoadScene("Lobby");
-    }
 
     void LateUpdate() // 보스 HP바
     {
@@ -41,39 +32,21 @@ public class GameManager : MonoBehaviour
         defeat.gameObject.SetActive(true);
     }
 
-    public void OnClickLobbyButton() // 로비 이동
+    public void OnClickLobbyButton()
     {
+        // "Lobby" 씬으로 이동
         SceneManager.LoadScene("Lobby");
     }
 
-    public void onClickCloseOptionUI() // 옵션 닫기
+    public void OnClickStage1Button()
     {
-        optionUI.gameObject.SetActive(false);
-    }
-
-    public void OnClickStage1Button() // stage1 이동
-    {
+        // "Stage1" 씬으로 이동
         SceneManager.LoadScene("Stage1");
     }
 
-    public void OnClickStage2Button() // stage2 이동
+    public void OnClickStage2Button()
     {
+        // "Stage2" 씬으로 이동
         SceneManager.LoadScene("Stage2");
-    }
-
-    public void OnClickOption()
-    {
-        optionUI.gameObject.SetActive(false);
-        optionSoundUI.gameObject.SetActive(true);
-    }
-    public void OnClickOption2()
-    {
-        optionUI.gameObject.SetActive(true);
-        optionSoundUI.gameObject.SetActive(false);
-    }
-
-public void OnClickGameQuit() // 게임 종료
-    {
-        Application.Quit();
     }
 }
